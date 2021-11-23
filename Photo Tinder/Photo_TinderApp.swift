33fee5e8be45eec2af9518a390502f1a4d05fd8e
Angelina Staeck <angelina.staeck@gmail.com>
2021-11-23 12:48:43 -0600

    project creation

---
+++ Photo Tinder/Photo_TinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_TinderApp.swift
+//  Photo Tinder
+//
+//  Created by Angelina Staeck on 23.11.21.
+//
+
+import SwiftUI
+
+@main
+struct Photo_TinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
