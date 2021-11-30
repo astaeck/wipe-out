47f08467b4259f7209b662f86752c12ca0c756ed
Angelina Staeck <angelina.staeck@gmail.com>
2021-11-30 14:19:41 -0600

    More button styling

--- Photo Tinder/CustomButtonStyle.swift
+++ Photo Tinder/CustomButtonStyle.swift
@@ -11,9 +11,10 @@ struct CustomButtonStyle: ButtonStyle {
   func makeBody(configuration: Configuration) -> some View {
     configuration.label
       .font(.headline)
+      .frame(width: 80, height: 45)
       .foregroundColor(.white)
-      .padding(10)
       .background(Color.blue)
+      .opacity(configuration.isPressed ? 0.8 : 1.0)
       .clipShape(RoundedRectangle(cornerRadius: 20))
   }
 }
