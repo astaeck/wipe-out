edbdb26421d3a428a5220584678c39eb0de0f873
Angelina Staeck <angelina.staeck@gmail.com>
2022-01-13 18:57:07 -0600

    Add caching to UserDefaults and retry to LocationLoader

--- Photo Tinder/LocationLoader.swift
+++ Photo Tinder/LocationLoader.swift
@@ -14,10 +14,14 @@ class LocationLoader: LoadableObject {
     @Published private(set) var state: LoadingState<String> = .idle
     private let geocoder: CLGeocoder
     private let location: CLLocation?
+    private let userDefaults: UserDefaults
 
-    init(location: CLLocation?, geocoder: CLGeocoder = CLGeocoder()) {
+    init(location: CLLocation?,
+         geocoder: CLGeocoder = CLGeocoder(),
+         userDefaults: UserDefaults = UserDefaults.standard) {
         self.location = location
         self.geocoder = geocoder
+        self.userDefaults = userDefaults
     }
     
     func load() {
@@ -31,9 +35,19 @@ class LocationLoader: LoadableObject {
     }
     
     private func getLocationName(location: CLLocation?, completion: @escaping (String) -> Void) {
-        guard let location = location else { return completion(" ") }
-        geocoder.reverseGeocodeLocation(location) { placemarks, error in
-            guard let placemark = placemarks?.first?.locality else { return completion(" ") }
+        guard let location = location else { return completion("YO") }
+        let locationKey = "\(location.coordinate.latitude)\(location.coordinate.longitude)"
+        if let cachedLocation = userDefaults.value(forKey: locationKey) as? String {
+            print("cached location")
+            return completion(cachedLocation)
+        }
+        print("reverse geocoding")
+        geocoder.reverseGeocodeLocation(location) { [weak self] placemarks, _ in
+            guard let placemark = placemarks?.first?.locality else {
+                self?.getLocationName(location: location, completion: completion)
+                return
+            }
+            self?.userDefaults.set(placemark, forKey: locationKey)
             completion(placemark)
         }
     }
