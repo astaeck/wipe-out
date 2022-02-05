d3b4a73c0a6a0874a977bf44eadd464774f9a42e
Angelina Staeck <angelina.staeck@gmail.com>
2022-02-04 18:54:48 -0600

    Convert images into data for later comparison

--- Photo Tinder/SimilarAssetsGrids/SimilarAssetsViewModel.swift
+++ Photo Tinder/SimilarAssetsGrids/SimilarAssetsViewModel.swift
@@ -6,18 +6,44 @@
 //
 
 import SwiftUI
+import Photos
 
 class SimilarAssetsViewModel: LoadableObject {
     typealias Output = [SimilarCollection]
     @Published private(set) var state: LoadingState<[SimilarCollection]> = .idle
-
+    private let imageManager: PHCachingImageManager
     private let cards: [Card]
-    
-    init(cards: [Card]) {
+    var imageData: [Data] = []
+    let group = DispatchGroup()
+
+    init(cards: [Card],
+         imageManager: PHCachingImageManager = PHCachingImageManager()) {
         self.cards = cards
+        self.imageManager = imageManager
     }
 
-    func load() {        
-        state = .loaded([SimilarCollection(cards: [cards[0], cards[1], cards[2]])])
+    func load() {
+//        state = .loaded([SimilarCollection(cards: [cards[0], cards[1], cards[2]])])
+        
+        let cards = [cards[0], cards[1], cards[2], cards[3]]
+    
+        cards.forEach { createData($0.asset) }
+        
+        group.notify(queue: DispatchQueue.main, execute: {
+
+            print("Finished all requests.")
+
+        })
+    }
+    
+    private func createData(_ asset: PHAsset) {
+        group.enter()
+
+        let resultHandler: (Data?, String?, CGImagePropertyOrientation, [AnyHashable : Any]?) -> Void = { data, _ , _ , _ in
+            guard let data = data  else { return }
+            self.imageData.append(data)
+            self.group.leave()
+        }
+        imageManager.requestImageDataAndOrientation(for: asset, options: nil, resultHandler: resultHandler)
     }
 }
