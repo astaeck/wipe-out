815ac03512d00c910b7620826662a47138997592
Angelina Staeck <angelina.staeck@gmail.com>
2022-06-29 14:59:33 +0200

    Filter out videos from similar photo grids

--- Wipe Out/SimilarAssetsGrids/SimilarAssetsLoader.swift
+++ Wipe Out/SimilarAssetsGrids/SimilarAssetsLoader.swift
@@ -11,11 +11,13 @@ import Photos
 class SimilarAssetsLoader {
     
     class func collectionsWith(_ cards: [Card]) -> [SimilarCollection] {
-        let cardsWithoutScreenshots = cards.filter { !$0.asset.mediaSubtypes.contains(.photoScreenshot) }
+        let cardsWithoutVideos = cards.filter { $0.asset.mediaType != .video }
+        let cardsWithoutScreenshots = cardsWithoutVideos.filter { !$0.asset.mediaSubtypes.contains(.photoScreenshot) }
         let groupedCards = Dictionary(grouping: cardsWithoutScreenshots.map { $0 }) { card -> DateComponents in
             return Calendar.current.dateComponents([.minute, .day, .year, .month], from: (card.asset.creationDate)!)
         }
         let similarGroupedCards = groupedCards.values.filter { $0.count > 2 }
+        print("Similar grids \(similarGroupedCards.count)")
         return similarGroupedCards.map { SimilarCollection(cards: $0) }
     }
 }
