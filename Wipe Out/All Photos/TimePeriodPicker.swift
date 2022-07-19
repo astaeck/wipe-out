79c6a0de99293615d9ed7f306d04ebf8dc1db0f8
Angelina Staeck <angelina.staeck@gmail.com>
2022-07-19 17:18:40 +0200

    Add `TimePeriodPicker`

---
+++ Wipe Out/All Photos/TimePeriodPicker.swift
@@ -0,0 +1,18 @@
+//
+//  TimePeriodPicker.swift
+//  Wipe Out
+//
+//  Created by Angelina Staeck on 19.07.22.
+//
+
+import SwiftUI
+
+struct TimePeriodPicker: View {
+    @EnvironmentObject var viewModel: CardsViewModel
+    @State var currentDate: Date = Date()
+
+    var body: some View {
+        DatePicker("", selection: $currentDate)
+            .datePickerStyle(.compact)
+    }
+}
